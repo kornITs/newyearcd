605bbc294fb0cfed4f9bf3b2d8bbf6578de017c1
kornITs <prattakorn.553@gmail.com>
2024-12-23 17:48:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function countdown() {
+    const newYear = new Date('January 1, 2025 00:00:00').getTime();
+    const now = new Date().getTime();
+    const distance = newYear - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(interval);
+        document.getElementById('countdown').innerHTML = "สวัสดีปีใหม่!";
+    }
+}
+
+const interval = setInterval(countdown, 1000);
